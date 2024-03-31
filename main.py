d6b796409dd1ad4935161cd8f45baf3349a80a3b
RinKirsan <116436137+RinKirsan@users.noreply.github.com>
2024-03-31 20:55:22 +0400

    update

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import pandas as pd
-
-ad_description = None
+from openpyxl import Workbook, load_workbook
 
 # Функция для сохранения данных в таблицу Excel
 def save_to_excel(text, photo_link):
-    df = pd.DataFrame({'Text': [text], 'PhotoLink': [photo_link]})
-    df.to_excel('реклама.xlsx', index=False, header=True)
+    wb = load_workbook('реклама.xlsx')
+    ws = wb.active
+    # Определение строки для записи
+    row_number = ws.max_row + 1
+    ws.cell(row=row_number, column=1).value = text
+    ws.cell(row=row_number, column=2).value = photo_link
+    wb.save('реклама.xlsx')
+
+
 
 # Функция для отправки сообщения
 def send_message(user_id, message):
@@ -19,16 +25,18 @@ def send_message(user_id, message):
 
 # Функция для обработки команды !рек
 def handle_command(user_id, command):
-    if command == '1':
+    try:
+        index = int(command)
         df = pd.read_excel('реклама.xlsx')
-        if not df.empty:
-            text = df.iloc[0]['Text']
-            photo_link = df.iloc[0]['PhotoLink']
+        if not df.empty and index <= len(df):
+            text = df.iloc[index - 1]['Text']
+            photo_link = df.iloc[index - 1]['PhotoLink']
             send_message(user_id, f"Текст рекламы: {text}\nСсылка на картинку: {photo_link}")
         else:
-            send_message(user_id, "В таблице нет сохраненных рекламных объявлений.")
-    else:
-        send_message(user_id, "Неизвестная команда.")
+            send_message(user_id, "Указанная строка не найдена.")
+    except ValueError:
+        send_message(user_id, "Необходимо указать номер строки после команды !рек.")
+
 
 
 # Функция для обработки рекламы
@@ -44,6 +52,8 @@ def handle_advertisement(user_id, event):
                     send_message(user_id, "Рекламное объявление сохранено.")
                     break
 
+
+
 # Авторизация в ВКонтакте
 token_file = open('C:\\1\\token.txt')
 vk_session = vk_api.VkApi(token=token_file.read())
@@ -51,18 +61,24 @@ token_file.close()
 vk = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
+def main():
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            if event.to_me:
 
-            msg =event.text.lower()
-            id = event.user_id
-            
-            if msg == '!рек':
-                command = msg.split()[1]
-                handle_command(id, command)
-            elif msg == '!нач':
-                send_message(id, "Введите описание и прикрепите фотографию.\nФотографию и описание следует отправлять вместе, одним сообщением.")
-                handle_advertisement(id, event)
-            elif msg == 'кон':
-                break
+                msg =event.text.lower()
+                id = event.user_id
+                
+                if event.text.lower().startswith('!рек'):
+                    command = msg.split()[1]
+                    send_message(id,command)
+                    handle_command(id, command)
+                elif msg =='!id':
+                    send_message(id,id)
+                elif msg == '!нач':
+                    send_message(id, "Введите описание и прикрепите фотографию.\nФотографию и описание следует отправлять вместе, одним сообщением.")
+                    handle_advertisement(id, event)
+                elif msg == 'кон':
+                    break
+
+main()
