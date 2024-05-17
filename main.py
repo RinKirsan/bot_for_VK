96c842a36533188aaeb10817f5c98282d2813df4
RinKirsan <116436137+RinKirsan@users.noreply.github.com>
2024-05-17 13:45:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def main():
                 else:
                     adminValid = False
 
-                if event.text.lower().startswith('!рек'):
+                if event.text.lower().startswith('!рек')& adminValid:
                     command = msg.split()[1]
                     handle_command(id, command)
                 elif (msg =='!id') & adminValid:
